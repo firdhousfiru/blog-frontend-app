a222b359fc1a002ce3e702a5a890a40067b6087c
firdhousfiru <firdhouskh2002@gmail.com>
2024-06-13 14:17:40 +0530

    signup base

---
+++ src/components/Signup.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-4">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+<label htmlFor="" className="form-label">NAME:</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">EMAIL:</label>
+                <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">PASSWORD:</label>
+                <input type="password" name="" id="" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                    <button className="btn btn-success">SUBMIT</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Signup
